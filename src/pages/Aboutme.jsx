96afb7092c9a193264a65265c0708d40394afda3
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2024-09-20 16:56:07 -0600

    added more to the writing portfolio, updated about me page

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -1,6 +1,4 @@
-//import pfp from "../assets/profilepic.png";
 import { useState, useEffect } from "react";
-//import Navbar from '../components/Navbar'
 
 export default function Aboutme() {
   const [showFirstParagraph, setShowFirstParagraph] = useState(false);
@@ -43,60 +41,117 @@ export default function Aboutme() {
     setIsSmallScreen(window.innerWidth <= 768);
   };
 
-  window.addEventListener("resize", checkScreenWidth);
-
   useEffect(() => {
-    checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
+    return () => window.removeEventListener("resize", checkScreenWidth);
   }, []);
 
   return (
     <div className={`flex flex-col h-screen overflow-y-auto bg-purple-100 ${!isSmallScreen ? "ml-[25%]" : "mt-10"}`}>
-  <style>
-    {`
-      /* Hide the scrollbar for WebKit browsers */
-      ::-webkit-scrollbar {
-        display: none;
-      }
-      /* Hide scrollbar for Firefox */
-      scrollbar-width: none;
-      /* Hide scrollbar for IE/Edge */
-      -ms-overflow-style: none;
-    `}
-  </style>
+      <style>
+        {`
+          /* Hide the scrollbar for WebKit browsers */
+          ::-webkit-scrollbar {
+            display: none;
+          }
+          /* Hide scrollbar for Firefox */
+          scrollbar-width: none;
+          /* Hide scrollbar for IE/Edge */
+          -ms-overflow-style: none;
+        `}
+      </style>
       <div className="flex justify-center mt-10">
         <div
           className={`flex flex-col items-center justify-center w-40 h-40 text-xl rounded-full bg-gradient-to-l from-pink-400 to-purple-400 cursor-pointer mx-4 ${
             firstParagraphBorder ? "bg-gradient-to-r from-pink-500 to-purple-500 w-44 h-44" : "w-40 h-40"
           } ${!isSmallScreen ? "w-40 h-40 text-xl" : "w-24 h-24 text-base"}`}
           onClick={toggleFirstParagraph}
-        ><span className="text-white text-center">Soft Skills</span></div>
+        >
+          <span className="text-white text-center">Resume</span>
+        </div>
         <div
           className={`flex flex-col items-center justify-center w-40 h-40 text-xl rounded-full bg-gradient-to-t from-pink-400 to-orange-400 cursor-pointer mx-4 ${
             secondParagraphBorder ? "bg-gradient-to-b from-pink-500 to-orange-500 w-44 h-44" : "w-40 h-40"
           } ${!isSmallScreen ? "w-40 h-40 text-xl" : "w-24 h-24 text-base"}`}
           onClick={toggleSecondParagraph}
-        ><span className="text-white text-center">About Me</span></div>
+        >
+          <span className="text-white text-center">About Me</span>
+        </div>
         <div
           className={`flex flex-col items-center justify-center w-40 h-40 text-xl rounded-full bg-gradient-to-l from-purple-400 to-pink-400 cursor-pointer mx-4 ${
             thirdParagraphBorder ? "bg-gradient-to-r from-purple-500 to-pink-500 w-44 h-44" : "w-40 h-40"
           } ${!isSmallScreen ? "w-40 h-40 text-xl" : "w-24 h-24 text-base"}`}
           onClick={toggleThirdParagraph}
-        ><span className="text-white text-center">Technical Skills</span></div>
+        >
+          <span className="text-white text-center">Technical Skills</span>
+        </div>
       </div>
+
       <div className="flex-1 p-4">
         {showFirstParagraph && (
-          <div className="text-lg mt-5 mx-auto w-3/4 text-center">
-            <h1 className="text-3xl mt-2 font-bold">Soft Skills</h1>
-            <ul className="text-2xl mt-2">
-              <li>• Time management</li>
-              <li>• Conflict resolution</li>
-              <li>• Collaborative & friendly</li>
-              <li>• Tenacious & determined</li>
-              <li>• Adaptable to swift changes</li>
-              <li>• Understanding business needs</li>
-              <li>• Maintain a positive attitude in tough situations</li>
-            </ul>
-        </div>
+          <div className="text-lg mt-5 mx-auto w-3/4">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              {/* Resume Content */}
+              <div className="bg-gradient-to-tr text-white from-orange-400 via-pink-400 to-purple-400 shadow-md rounded px-8 pt-6 pb-4 mb-4">
+                <h1 className="text-2xl font-bold">Kaelyn Patsy-Jean Rasmussen</h1>
+                <p className="text-xl">Writer & Developer || Ogden, UT</p>
+              </div>
+
+              <div className="mb-6 font-light text-base">
+                <h2 className="text-xl font-bold underline">Education</h2>
+                <div className="m-4">
+                  <p className="font-bold">Full-Stack Bootcamp Certification, University of Utah, 2023</p>
+                  <p>• M.E.R.N. Stack, JavaScript, GraphQL, Apollo, MySQL, JWT, Git</p>
+                  <p className="font-bold">High School Diploma, Two Rivers High School, 2018</p>
+                  <p>• Valedictorian, High Honors</p>
+                </div>
+              </div>
+
+              <div className="mb-6 font-light text-base">
+                <h2 className="text-xl font-bold underline">Work History</h2>
+                <div className="m-4">
+                  <p className="font-bold text-lg">FedEx Ground - Owens Transportation</p>
+                  <p className="font-medium text-base">Delivery Driver | 02/2022 - Present</p>
+                  <p>• Managed efficient delivery routes for 100-300 packages daily.</p>
+                  <p>• Handled package logistics, including pickup and delivery coordination.</p>
+                  <p>• Assisted in vehicle maintenance and supported team members in route completion.</p>
+                </div>
+
+                <div className="m-4">
+                  <p className="font-bold text-lg">Amazon Delivery - Top Flight Logistics</p>
+                  <p className="font-medium text-base">Delivery Driver | 08/2021 - 02/2022</p>
+                  <p>• Coordinated and executed delivery of 200-350 packages daily within tight schedules.</p>
+                  <p>• Provided customer-specific delivery services and assisted fellow drivers.</p>
+                </div>
+
+                <div className="m-4">
+                  <p className="font-bold text-lg">Wayfair</p>
+                  <p className="font-medium text-base">Customer Service Rep | 08/2020 - 08/2021</p>
+                  <p>• Addressed online order issues, tracked packages, and managed customer concerns.</p>
+                  <p>• Contributed to internal data management and customer satisfaction initiatives.</p>
+                </div>
+
+                <div className="m-4">
+                  <p className="font-bold text-lg">Humphrey Plumbing Heating and Air & Express Home Services</p>
+                  <p className="font-medium text-base">Office Administrator | 09/2019 - 06/2020</p>
+                  <p>• Scheduled appointments and managed service requests.</p>
+                  <p>• Supported website content management and handled confidential documents.</p>
+                </div>
+              </div>
+
+              <div className="mb-6">
+                <h2 className="text-xl font-bold underline">Additional Experience</h2>
+                <div className="m-4">
+                  <p className="font-bold text-lg">Savers <span className="font-medium text-base">Retail Associate | 05/2017 - 05/2019</span></p>
+                  <p className="font-bold text-lg">Firehouse Subs <span className="font-medium text-base">Crew Member | 08/2016 - 05/2017</span></p>
+                </div>
+              </div>
+
+              <div className="font-light text-base">
+                <h2 className="text-lg font-bold">References provided upon request</h2>
+              </div>
+            </div>
+          </div>
         )}
         {showSecondParagraph && (
           <div className="mt-5 mx-auto w-3/4 text-center">
